4d31726a61f6a9a7d00d508f6149744373d0ab5d
片岡望実 <kataokanozomujitsu@kataokanozomujitsunoMacBook-Pro.local>
2023-08-08 18:53:07 +0900

    First commit

--- src/index.js
+++ src/index.js
@@ -15,3 +15,20 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const GACHA_ITEMS = [
+  { name: "Normal Dragon", rarity: "Normal", probability: 0.6 },
+  { name: "Rare Dragon", rarity: "Rare", probability: 0.3 },
+  { name: "Super Rare Dragon", rarity: "Super Rare", probability: 0.09 },
+  { name: "Ultra Rare Dragon", rarity: "Ultra Rare", probability: 0.01 },
+];
+
+function drawGacha() {
+  const rand = Math.random();
+  let sum = 0;
+
+  for (const item of GACHA_ITEMS) {
+    sum += item.probability;
+    if (rand < sum) return item;
+  }
+}
